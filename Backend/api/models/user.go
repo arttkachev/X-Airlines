622c40a24dc57abe6fd232b48d0933892e66ccaa
arttkachev <art.tkachev@bk.ru>
2021-10-17 01:41:47 +0300

    Backend. First endpoints test. Workspace transition from $GOPATH to Modules. User model update

--- Backend/api/models/user.go
+++ Backend/api/models/user.go
@@ -2,10 +2,11 @@ package models
 
 // user model
 type User struct {
+	ID       string   `json:"id"`
 	Name     string   `json:"name"`
 	Email    string   `json:"email"`
 	Password string   `json:"password"`
 	IsAdmin  bool     `json:"isAdmin"`
 	Balance  int      `json:"balance"`
-	Fleet    []string `json:"fleet"`
+	Airlines []string `json:"airlines"`
 }
--- Backend/server.go
+++ Backend/server.go
@@ -4,11 +4,14 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"time"
 
+	"github.com/arttkachev/X-Airlines/Backend/api/models"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/rs/xid"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -16,12 +19,34 @@ import (
 
 const port = ":3000"
 
+var users []models.User
+
 // Endpoints
 func Welcome(c *gin.Context) {
 	c.JSON(200, gin.H{"Message": "Welcome to X-Airlines"})
 }
 
+func AddUser(c *gin.Context) {
+	var user models.User
+	err := c.ShouldBindJSON(&user) // ShouldBindJSON marshals the incoming request body into a struct passed in as an argument (it's user in our case)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+
+	user.ID = xid.New().String() // xid lib set a unique ID
+	users = append(users, user)
+	c.JSON(http.StatusOK, user) // sends a response with httpStatusOK and a newly created user as a JSON
+}
+
+func GetUsers(c *gin.Context) {
+	c.JSON(http.StatusOK, users) //marshals the users array to JSON
+}
+
 func main() {
+
+	users = make([]models.User, 0)
 
 	// load .env file
 	err := godotenv.Load()
@@ -64,8 +89,10 @@ func main() {
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.New()
 
-	// handler of a main page
+	// handlers
 	router.GET("/", Welcome)
+	router.POST("/users", AddUser)
+	router.GET("/users", GetUsers)
 
 	// listen and serve
 	router.Run(port)
