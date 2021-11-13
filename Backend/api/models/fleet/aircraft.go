66692f619db386e287e1cfa51ce4600b6034f162
arttkachev <art.tkachev@bk.ru>
2021-11-13 19:44:04 +0300

    Backend. Aircraft model

--- Backend/api/models/fleet/aircraft.go
+++ Backend/api/models/fleet/aircraft.go
@@ -1,20 +1,21 @@
 package fleet
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 // aircraft model
 type Aircraft struct {
-	Name                string   `json:"name"`
-	Icon                string   `json:"icon"`
-	Gallery             []string `json:"gallery"`
-	General             string   `json:"general"`             // TODO: AircraftGeneral Object id
-	Airframe            string   `json:"airframe"`            // TODO: Airframe Object id
-	Engines             []string `json:"engines"`             // TODO: Engine Object id
-	APU                 string   `json:"apu"`                 // TODO: APU Object id
-	Avionics            string   `json:"avionics"`            // TODO: Avionics Object id
-	AdditionalEquipment string   `json:"additionalEquipment"` // TODO: AdditionalEquipment Object id
-	Exterior            string   `json:"exterior"`            // TODO: Exterior Object id
-	Interior            string   `json:"interior"`            // TODO: Interior Object id
-	Performance         string   `json:"performance"`         // TODO: Performance Object id
-	Price               int      `json:"price"`
-	SellerInfo          string   `json:"sellerInfo"` // TODO: SellerInfo Object id
-	Tags                []string `json:"tags"`
+	Name                string               `json:"name" bson:"name`
+	Icon                string               `json:"icon" bson:"icon`
+	General             string               `json:"general" bson:"general`
+	Airframe            primitive.ObjectID   `json:"airframe" bson:"airframe`
+	Engines             []primitive.ObjectID `json:"engines" bson:"engines`
+	APU                 primitive.ObjectID   `json:"apu" bson:"apu`
+	Avionics            primitive.ObjectID   `json:"avionics" bson:"avionics`
+	Exterior            primitive.ObjectID   `json:"exterior" bson:"name exterior`
+	Interior            primitive.ObjectID   `json:"interior" bson:"interior`
+	Performance         primitive.ObjectID   `json:"performance" bson:"performance`
+	AdditionalEquipment primitive.ObjectID   `json:"additionalEquipment" bson:"additionalEquipment`
+	Price               float32              `json:"price" bson:"price`
+	Seller              primitive.ObjectID   `json:"sellerInfo" bson:"seller`
+	Tags                []string             `json:"tags" bson:"tags`
 }
